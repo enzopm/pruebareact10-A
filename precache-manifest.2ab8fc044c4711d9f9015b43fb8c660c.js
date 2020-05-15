4246e66cbe8feaa5efb3c60921857f00896f2da8
Enzo Paladines <GPDISVA108@grupopalmon.com>
2020-05-15 12:15:16 -0500

    Updates

---
+++ precache-manifest.2ab8fc044c4711d9f9015b43fb8c660c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a01644c87e75207a656819bd310fc1ff",
+    "url": "/pruebareact10-A/index.html"
+  },
+  {
+    "revision": "db9db04c7f20e0748812",
+    "url": "/pruebareact10-A/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6b4abd01626ecaabc78b",
+    "url": "/pruebareact10-A/static/js/2.b0cebe45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pruebareact10-A/static/js/2.b0cebe45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db9db04c7f20e0748812",
+    "url": "/pruebareact10-A/static/js/main.7d673fc7.chunk.js"
+  },
+  {
+    "revision": "3e752ab851964a7a9a4a",
+    "url": "/pruebareact10-A/static/js/runtime-main.758c16f8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pruebareact10-A/static/media/logo.5d5d9eef.svg"
+  }
+]);
